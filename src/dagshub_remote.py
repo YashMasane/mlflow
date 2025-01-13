1e69d6174813f93f6d0f5d5096f8f8824b305635
YashMasane <masaneyash6@gmail.com>
2025-01-13 22:46:14 +0530

    mlflow with autolog, dagshub and hypertune

---
+++ src/dagshub_remote.py
@@ -0,0 +1,57 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+
+import dagshub
+dagshub.init(repo_owner='masaneyash6', repo_name='mlflow', mlflow=True)
+mlflow.set_tracking_uri(uri='https://dagshub.com/masaneyash6/mlflow.mlflow')
+
+wine = load_wine()
+
+x = wine.data
+y = wine.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+max_depth = 15
+n_estimators = 20
+
+mlflow.set_experiment('MLflow practice')
+
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
+    rf.fit(x_train, y_train)
+
+    y_pred = rf.predict(x_test)
+    accuracy = accuracy_score(y_pred, y_test)
+
+    mlflow.log_metric('accuracy', accuracy)
+    mlflow.log_param('max_depth', max_depth)
+    mlflow.log_param('n_estimators', n_estimators)
+
+    # Creating a confusion matrix plot
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=wine.target_names, yticklabels=wine.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+
+    # save plot
+    plt.savefig("Confusion-matrix.png")
+
+    mlflow.log_artifact('confusion-matrix.png')
+    mlflow.log_artifact(__file__)
+
+    
+    # tags
+    mlflow.set_tags({"Author": 'Yash', "Project": "Wine Classification"})
+
+    mlflow.sklearn.log_model(rf, 'random forest classifier')
+
+    print(accuracy)
